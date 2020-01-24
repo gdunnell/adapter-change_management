b60554cfe5fd3ae4f83734c973461c5a1b6ac2b3
gdunnell <gdunnell@hotmail.com>
2020-01-24 19:03:14 +0000

    Lab 2

--- main.js
+++ main.js
@@ -69,6 +69,33 @@ function get(serviceNowTable, callback) {
     uri: `/api/now/table/${serviceNowTable}?sysparm_limit=1`,
   };
 
+  // Send Request to ServiceNow.
+  // We are passing variable requestOptions for the first argument.
+  // We are passing an anonymous function, an error-first callback,
+  // for the second argument.
+  request(requestOptions, (error, response, body) => {
+    /**
+     * Process ServiceNow error, response and body.
+     * Check error and response code to make sure
+     * response is good.
+     */
+    if (error) {
+      console.error('Error present.');
+      callbackError = error;
+    } else if (!validResponseRegex.test(response.statusCode)) {
+      console.error('Bad response code.');
+      callbackError = response;
+    } else if (response.body.includes('Hibernating Instance')) {
+      callbackError = 'Service Now instance is hibernating';
+      console.error(callbackError);
+    } else {
+      callbackData = response;
+    }
+    return callback(callbackData, callbackError);
+  });
+
+}
+
 /**
  * @function post
  * @description Call the ServiceNow POST API.
@@ -101,7 +128,6 @@ function post(serviceNowTable, callback) {
     uri: `/api/now/table/${serviceNowTable}`,
   };
 
-
   // Send Request to ServiceNow.
   // We are passing variable requestOptions for the first argument.
   // We are passing an anonymous function, an error-first callback,
@@ -128,7 +154,6 @@ function post(serviceNowTable, callback) {
   });
 
 }
-
 
 /*
  * This section is used to test your project.
